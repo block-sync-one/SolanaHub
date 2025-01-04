6187bf655e314b7a88cea895ca39614d5cd8509d
AC-devon <inbox@avaulto.com>
2025-01-04 17:39:24 +0200

    update turnstile client

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -372,12 +372,17 @@ export class PortfolioService {
     if (this._platforms.length) {
       return this._platforms
     }
-
+    const vrs = this._vrs.localStorage.getData('platforms')
+    if(vrs){
+      this._platforms = JSON.parse(vrs)
+      return this._platforms
+    }
     try {
       this._platforms = await (await fetch(`${this.restAPI}/api/portfolio/platforms`)).json();
     } catch (error) {
       console.warn(error)
     }
+    this._vrs.localStorage.saveData('platforms', JSON.stringify(this._platforms))
     return this._platforms
   }
 
