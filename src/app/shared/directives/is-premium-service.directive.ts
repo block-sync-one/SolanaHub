4c08fda09f06c5ee5c4cd527cb0ab747b5d18fa7
Collins Kamgaing <collinskamgaing@gmail.com>
2025-01-08 19:12:33 +0100

    feat(): Implement modal footer for premium actions

---
+++ src/app/shared/directives/is-premium-service.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, inject, Input, OnInit, TemplateRef, ViewContainerRef } from "@angular/core";
+import { FreemiumService } from "../layouts/freemium/freemium.service";
+
+@Directive({
+  selector: "[appIsPremiumAction]",
+  standalone: true,
+})
+export class IsPremiumServiceDirective implements OnInit {
+  @Input("appIsPremiumAction") action: string;
+  private readonly templateRef = inject(TemplateRef);
+  private readonly freemiumService = inject(FreemiumService);
+  private readonly viewContainer = inject(ViewContainerRef);
+
+  ngOnInit() {
+    this.action = this.action.split("-modal")[0];
+
+    console.log("Action", this.action)
+    if (this.freemiumService.isPremiumAction(this.action)) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
+    }
+  }
+}
